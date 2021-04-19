1e0b41ba460edcc951523fc634c6b4531ef882ee
Ashish Kumar Singh <62392863+ashishsingh877@users.noreply.github.com>
2021-04-20 03:59:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ api = tweepy.API(authenticate, wait_on_rate_limit = True)
 def app():
 
 
-	st.title("Tweet Analyzer 🔥")
+	st.title("Tweet Analyzer")
 
 
 	activities=["Tweet Analyzer","Generate Twitter Data"]
